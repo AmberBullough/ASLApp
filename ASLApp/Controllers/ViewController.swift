936b54895e5d0876e97fd2403d6d0592ba218e24
Amber Bullough <amberbullough@rainicorn.bertbullough.com>
2017-11-23 20:30:57 -0700

    Created project, added in groups, and added color tools

--- ASLApp/Controllers/ViewController.swift
+++ ASLApp/Controllers/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+  lazy var colorTool : ColorTools = ColorTools()
+        
+      
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
