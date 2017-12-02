5bffd5514597acac7ecf32abab1142bd12df4e49
Amber Bullough <amberbullough@rainicorn.bertbullough.com>
2017-12-01 20:45:59 -0700

    Added in code for the video, the video, then tried to run it. Ended up with an error

--- ASLApp/Controllers/AnimalViewController.swift
+++ ASLApp/Controllers/AnimalViewController.swift
@@ -7,10 +7,50 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+
 
 class AnimalViewController : UIViewController
 {
     lazy var colorTool : ColorTools = ColorTools()
+
+    @IBOutlet weak var TigerVideo: UIButton!
+    @IBAction func TigerVideoButton(_ sender: Any)
+    { if let path = Bundle.main.path(forResource: "Tiger",ofType: "mov")
+    {
+        let video = AVPlayer(url: URL(fileURLWithPath: path))
+        let videoPlayer = AVPlayerViewController()
+        videoPlayer.player = video
+        
+        present(videoPlayer, animated: true, completion:
+            {
+                video.play()
+        })
+        
+    }
+//    override func viewDidAppear(_ animated: Bool)
+//    {
+//        super.viewDidAppear(animated)
+//        playVideo()
+//    }
+//
+//    private func playVideo()
+//    {
+//        guard let path = Bundle.main.path(forResource: "Tiger",ofType: "mov")
+//        else
+//        {
+//            debugPrint("Tiger.mov not found")
+//            return
+//        }
+//        let player = AVPlayer(url: URL(fileURLWithPath: path))
+//        let playerContoller = AVPlayerViewController()
+//        playerContoller.player = player
+//        present(playerContoller, animated: true)
+//        {
+//            player.play()
+//        }
+//    }
     
-    
+}
 }
